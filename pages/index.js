bced7ee9b31430c9669888f9ed03825e239f7d1b
fixmylifedesigns <ijd.irving@gmail.com>
2019-09-01 14:24:34 -0400

    cleaning style

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Head from "next/head";
-import NavBar from "../components/navigation";
 
 const Index = () => {
   return (
@@ -9,25 +8,21 @@ const Index = () => {
         <title>My styled page</title>
       </Head>
       <div className="navigationContainer">
-        <Link href="/">
-          <img
-            src="https://github.com/brokenulock/frontend/blob/master/src/bulfmlimg/navlogo.png?raw=true"
-            alt=""
-            className="navLogo"
-          />
+        <Link href="https://www.duranirving.com/">
+          <h1 className="portfolio">DuranIrving.com</h1>
         </Link>
 
         <nav className="navigation">
           <a
             href="https://documenter.getpostman.com/view/7133880/SVfMUAdJ?version=latest"
-            className="footerLink"
+            className="navLink"
             style={{ marginRight: "15px" }}
           >
             Api Doc
           </a>
           <a
-            href="https://github.com/brokenulock"
-            className="footerLink"
+            href="https://github.com/fixmylife-boilerplates/next-node-api"
+            className="navLink"
             style={{ marginRight: "15px" }}
           >
             Github Repo
@@ -47,14 +42,16 @@ const Index = () => {
             </div>
             <div className="topTextContainer">
               <h3>
-                <section className="introText">
-                  Bringing the community together to help locate stolen
-                  bicycles.
-                </section>
-                <section>SINCE 2016</section>
+                <h2 className="introText">
+                  Client Plus Server Api Boilerplate<br></br>
+                  Powered with Next.js
+                </h2>
               </h3>
-              <a href="https://www.brokenulock.com" className="footerLink">
-                Download Boilerplate
+              <a
+                href="https://github.com/fixmylifedesigns/next-node-api/archive/master.zip"
+                className="navLink"
+              >
+                <h3>Download Boilerplate</h3>
               </a>
             </div>
           </div>
@@ -62,7 +59,6 @@ const Index = () => {
         <div className="section" id="section1">
           <div className="sectionContent">
             <div className="imgContainer">
-              {/* className="illustration" */}
               <iframe
                 width="560"
                 height="315"
@@ -73,15 +69,16 @@ const Index = () => {
               ></iframe>
             </div>
             <div className="textContainer">
-              <h5>
-                Jimmy stepped inside a coffee shop to get a drink. He was only
-                gone for 10 minutes. When he came outside he realized his bike
-                had been stolen.
-              </h5>
+              <h3>
+                This tutorial shows how to get started with my node api
+                boilerplate. If you downloaded this then you are probably
+                looking to use next.js. this tutorial still works the same. Only
+                main difference is a new command added to scripts.
+              </h3>
             </div>
           </div>
         </div>
-        {/* <div className="section">
+         {/* <div className="section">
           <div className="sectionContent ContentReverse">
             <div className="imgContainer">
               <img
@@ -98,154 +95,50 @@ const Index = () => {
               </h5>
             </div>
           </div>
-        </div>
-        <div className="section">
-          <div className="sectionContent">
-            <div className="imgContainer">
-              <img
-                src="https://github.com/brokenulock/frontend/blob/master/src/bulfmlimg/design3.png?raw=true"
-                className="illustration"
-                alt=""
-              />
-            </div>
-            <div className="textContainer">
-              <h5>
-                A couple of days later, Rocco is on his way home and notices
-                someone riding a bike that he remembers seeing on
-                brokenulock.com
-              </h5>
-            </div>{" "}
-          </div>
-        </div>
-        <div className="section">
-          <div className="sectionContent ContentReverse">
-            <div className="imgContainer">
-              <img
-                src="https://github.com/brokenulock/frontend/blob/master/src/bulfmlimg/design4.png?raw=true"
-                className="illustration"
-                alt=""
-              />
-            </div>
-            <div className="textContainer">
-              <h5>
-                Rocco quickly opens up the web app, finds Jimmy's post and in
-                seconds he is able to contact Jimmy to let him know he just
-                spotted someone with his bike going down the block.
-              </h5>
-            </div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="sectionContent">
-            <div className="imgContainer">
-              <img
-                src="https://github.com/brokenulock/frontend/blob/master/src/bulfmlimg/design5.png?raw=true"
-                className="illustration"
-                alt=""
-              />
-            </div>
-            <div className="textContainer">
-              <h5>
-                Jimmy was able to search the last known location of his bike and
-                take the proper actions to retrieve it. All thanks to the
-                community.
-              </h5>
-            </div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="sectionContent ContentReverse">
-            <div className="imgContainer">
-              <img
-                src="https://github.com/brokenulock/frontend/blob/master/src/bulfmlimg/design6.png?raw=true"
-                className="illustration"
-                alt=""
-              />
-            </div>
-            <div className="textContainer">
-              <h5>
-                This Web App is being designed with the idea to help New Yorkers
-                retrieve their bikes. But there is nothing stopping us from
-                helping others worldwide.
-              </h5>
-            </div>
-          </div>
-        </div>
-       */}
+        </div> */}
+
+       
       </div>
-      <style jsx>
+      <style jsx global>
         {`
-          /* Navigation */
+        h1 h2 h3 h4 h5 h6 a p{
+          font-family: Impact, Charcoal, sans-serif
+        }
+
           .navigationContainer {
             margin: auto;
             width: 90%;
-            // position: fixed;
-            background: #33b8f3;
+            background: #282C34;
             padding: 10px 5% ;
             display: flex;
             flex-direction: row;
             justify-content: space-between;
           }
-
-          .navLogo {
-            width: 300px;
+          .navLink {
+            color: white;
+            font-weight: bold;
           }
-          .navLogo:hover {
+          .navLink:hover {
             cursor: pointer;
           }
+
+          .portfolio{
+            color:white;
+          }
+
           .navigation {
             display: flex;
-            /* flex-direction: row; */
+            align-items:center;
           }
 
-          .navImgContainer {
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            margin: 40px 0;
-          }
-          .mobileLogo {
-            margin: auto;
-            width: 250px;
-          }
-
-          .fa-bars {
-            font-size: 40px;
-            color: white;
-            display: none;
-          }
-          .fa-bars:hover {
-            cursor: pointer;
-          }
-          .fa-chevron-circle-down {
-            display: none;
-            font-size: 40px;
-            color: white;
-          }
-          .fa-chevron-circle-down:hover {
-            cursor: pointer;
-          }
-
-          .menuFont {
-            font-size: 20px;
-            font-weight: bold;
-            color: grey;
-          }
-
-          /* Home Page */
           .homeContainer {
             width: 100%; 
             display: flex;
-        flex-direction: column;
-        justify-content: center; */
+            flex-direction: column;
+            justify-content: center; */
           }
 
           .section {
-            /* margin: auto; */
-            /* max-height: 300px;  */
-            /* width: 100%; */
-            /* align-content: center; */
-            /* display: flex; */
             background: white;
           }
           .section:nth-child(odd) {
@@ -253,9 +146,8 @@ const Index = () => {
           }
 
           .section:first-child {
-            padding-top: 30px;
-            height: 100vh;
-            background: #33b8f3;
+            padding: 30px 0;
+            background: #282C34;
           }
 
           .topMain {
@@ -279,29 +171,6 @@ const Index = () => {
             color: white;
             text-align: center;
           }
-
-          .topButton {
-            background: #9b9b9b;
-            border: #9b9b9b;
-            font-weight: bold;
-          }
-
-          .fa-arrow-down {
-            color: #9b9b9b;
-            margin: auto;
-          }
-          .fa-arrow-down:hover {
-            cursor: pointer;
-          }
-
-          .bottomButton {
-            height: 5%;
-            /* padding-top:0.5%; */
-            background: white;
-            display: flex;
-            justify-content: center;
-            align-content: center;
-          }
           .sectionContent {
             margin: auto;
             padding: 10px;
@@ -324,13 +193,6 @@ const Index = () => {
             display: flex;
             align-items: center;
           }
-          .illustration {
-            padding: 10px;
-            width: 90%;
-            margin: auto;
-            /* height:100px; */
-            /* height: auto; */
-          }
           @media only screen and (max-width: 900px) {
             .sectionContent {
               padding: 0;
@@ -339,13 +201,10 @@ const Index = () => {
           }
 
           @media only screen and (max-width: 700px) {
-            .navigation {
-              display: none;
+            .topMain{
+              width:100%;
+              flex-direction: column;
             }
-            .fa-bars {
-              display: block;
-            }
-            /*  */
             .sectionContent {
               padding: 10px 0;
               width: 100%;
@@ -359,11 +218,8 @@ const Index = () => {
             .imgContainer {
               width: 90%;
             }
-            /*  */
             .HomePageLogoContainer {
               padding: 30px;
-            }
-            .sectionContent {
             }
           }
 
@@ -373,14 +229,9 @@ const Index = () => {
               align-items: center;
               justify-content: center;
             }
-            .fa-bars {
-              display: none;
-            }
-            .fa-chevron-circle-down {
-              display: block;
-            }
             .topMain {
               flex-direction: column;
+              height: 90%;
             }
             .HomePageLogoContainer {
               padding: 10px 0px;
@@ -389,12 +240,6 @@ const Index = () => {
             .topTextContainer {
               margin: 0px;
               width: 95%;
-            }
-            .topMain {
-              height: 90%;
-            }
-            .bottomButton {
-              height: 10%;
             }
             .sectionContent {
               flex-direction: column;
@@ -409,57 +254,10 @@ const Index = () => {
           }
 
           @media only screen and (max-width: 350px) {
-            .navLogo {
-              width: 100%;
-            }
             .introText {
               display: none;
             }
-            .topButton {
-              display: none;
-            }
-          }
-          /* Footer */
-          .footerContainer {
-            min-height: 40px;
-            padding: 5px;
-            text-align: center;
-            background: #33b8f3;
-            font-weight: bold;
-            color: white;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-around;
-          }
-          .iconLinks {
-            color: white;
-            /* font-weight: bold; */
-            font-size: 25px;
-            padding: 0px 4px;
-          }
-          .iconLinks:hover {
-            cursor: pointer;
-          }
-          .footerLink {
-            color: white;
-            font-weight: bold;
-          }
-          .footerLink:hover {
-            cursor: pointer;
-          }
 
-          @media only screen and (max-width: 450px) {
-            .footerContainer {
-              min-height: 40px;
-              padding: 10px;
-              text-align: center;
-              flex-direction: column;
-              flex-wrap: nowrap;
-              justify-content: center;
-            }
-          }
-
-         
           }
         `}
       </style>
